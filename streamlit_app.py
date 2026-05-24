1a1f713ed7ee3037dfc880d813c4c496ae8ddd9b
renalgobaywiyai-ops <renalgobaywiyai@gmail.com>
2026-05-24 10:01:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ with col1:
 st.header("Hubungi Kami")
 nama = st.text_input("Nama Lengkap")
 pesan = st.text_area("Tulis pesan Anda disini")
-if st.button("kirim Pesan")
+if st.button("kirim Pesan"):
     st.success(f"Terima Kasih {nama}! Pesan Anda
 telah terkirim. Kami akan merespon Secepatnya.")
 # Footer
