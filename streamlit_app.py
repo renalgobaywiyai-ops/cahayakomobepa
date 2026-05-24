b2b84d9c952055112598f67433be6e05de29f0f7
renalgobaywiyai-ops <renalgobaywiyai@gmail.com>
2026-05-24 10:15:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,19 +22,22 @@ pengunjung.
 """)
 # Bagian Layanan / Fitur
 st.header("Apa yang Kami Sajikan?")
-col1, = st.columns(1)
+col1, = st.columns(3)
 with col1:
-   st.subheader("Informasi")
+   st.subheader("📚Informasi")
    st.write("Berbagai artikel dan pengetahuan seputar topik yang bermanfaat untuk Anda.")
+with col2:
+    st.subheader("🎨 Karya Kreatif")
+    st.write("Kreatife dari tim Cahaya Komobepa.")
+with col3:
+    st.subheader("🤝 Kolaborasi")
+    st.write("Kerja sama dan berbagai gagasan bersama Anda.")
 # Bagi Kontak
-st.header("Hubungi Kami")
+st.header("📞Hubungi Kami")
 nama = st.text_input("Nama Lengkap")
 pesan = st.text_area("Tulis pesan Anda disini")
 if st.button("kirim Pesan"):
-    st.success(f"Terima Kasih {nama}! Pesan Anda")
-telah terkirim. Kami akan merespon Secepatnya.")
+    st.success(f"Terima Kasih {nama}! Pesan Anda") telah terkirim. Kami akan merespon Secepatnya.")
 # Footer
 st.markdwon("---")
-st.markdwon("<center> 2026 Cahaya Komobepa -
-Semua Hak Dilindungi<center>",
-unsafe_allow_html=True)
+st.markdwon("<center>© 2026 Cahaya Komobepa - Semua Hak Dilindungi<center>"©, unsafe_allow_html=True)
